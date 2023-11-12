033bc1e8761e9fa7f24c941f55c985cd9dbb8a75
William Pederzoli <wpederzoli@gmail.com>
2023-11-12 11:14:24 +0100

    Display format implementation

--- src/matrices/mod.rs
+++ src/matrices/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 pub struct Matrix3x3 {
     pub data: [f32; 9],
@@ -24,6 +26,24 @@ impl Matrix3x3 {
 impl Default for Matrix3x3 {
     fn default() -> Self {
         Matrix3x3 { data: [0.; 9] }
+    }
+}
+
+impl Display for Matrix3x3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "\n| {}, {}, {} |\n| {}, {}, {} |\n| {}, {}, {} |",
+            self.data[0],
+            self.data[3],
+            self.data[6],
+            self.data[1],
+            self.data[4],
+            self.data[7],
+            self.data[2],
+            self.data[5],
+            self.data[8]
+        )
     }
 }
 
