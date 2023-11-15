3ce0772c615718601fe29b6c2c9fbf3ccd2cbb3a
William Pederzoli <wpederzoli@gmail.com>
2023-11-15 11:23:11 +0100

    Matrix multiplication and multi assign. Tests

--- src/matrices/mod.rs
+++ src/matrices/mod.rs
@@ -100,5 +100,64 @@ impl MulAssign<f32> for Matrix3x3 {
     }
 }
 
+impl Mul<Matrix3x3> for Matrix3x3 {
+    type Output = Matrix3x3;
+    fn mul(self, rhs: Matrix3x3) -> Self::Output {
+        let m0 =
+            self.data[0] * rhs.data[0] + self.data[1] * rhs.data[3] + self.data[2] * rhs.data[6];
+        let m1 =
+            self.data[3] * rhs.data[0] + self.data[4] * rhs.data[3] + self.data[5] * rhs.data[6];
+        let m2 =
+            self.data[6] * rhs.data[0] + self.data[7] * rhs.data[3] + self.data[8] * rhs.data[6];
+        let m3 =
+            self.data[0] * rhs.data[1] + self.data[1] * rhs.data[4] + self.data[2] * rhs.data[7];
+        let m4 =
+            self.data[3] * rhs.data[1] + self.data[4] * rhs.data[4] + self.data[5] * rhs.data[7];
+        let m5 =
+            self.data[6] * rhs.data[1] + self.data[7] * rhs.data[4] + self.data[8] * rhs.data[7];
+        let m6 =
+            self.data[0] * rhs.data[2] + self.data[1] * rhs.data[5] + self.data[2] * rhs.data[8];
+        let m7 =
+            self.data[3] * rhs.data[2] + self.data[4] * rhs.data[5] + self.data[5] * rhs.data[8];
+        let m8 =
+            self.data[6] * rhs.data[2] + self.data[7] * rhs.data[5] + self.data[8] * rhs.data[8];
+
+        Matrix3x3::new(m0, m1, m2, m3, m4, m5, m6, m7, m8)
+    }
+}
+
+impl MulAssign<Matrix3x3> for Matrix3x3 {
+    fn mul_assign(&mut self, rhs: Matrix3x3) {
+        let m0 =
+            self.data[0] * rhs.data[0] + self.data[1] * rhs.data[3] + self.data[2] * rhs.data[6];
+        let m1 =
+            self.data[3] * rhs.data[0] + self.data[4] * rhs.data[3] + self.data[5] * rhs.data[6];
+        let m2 =
+            self.data[6] * rhs.data[0] + self.data[7] * rhs.data[3] + self.data[8] * rhs.data[6];
+        let m3 =
+            self.data[0] * rhs.data[1] + self.data[1] * rhs.data[4] + self.data[2] * rhs.data[7];
+        let m4 =
+            self.data[3] * rhs.data[1] + self.data[4] * rhs.data[4] + self.data[5] * rhs.data[7];
+        let m5 =
+            self.data[6] * rhs.data[1] + self.data[7] * rhs.data[4] + self.data[8] * rhs.data[7];
+        let m6 =
+            self.data[0] * rhs.data[2] + self.data[1] * rhs.data[5] + self.data[2] * rhs.data[8];
+        let m7 =
+            self.data[3] * rhs.data[2] + self.data[4] * rhs.data[5] + self.data[5] * rhs.data[8];
+        let m8 =
+            self.data[6] * rhs.data[2] + self.data[7] * rhs.data[5] + self.data[8] * rhs.data[8];
+
+        self.data[0] = m0;
+        self.data[3] = m1;
+        self.data[6] = m2;
+        self.data[1] = m3;
+        self.data[4] = m4;
+        self.data[7] = m5;
+        self.data[2] = m6;
+        self.data[5] = m7;
+        self.data[8] = m8;
+    }
+}
+
 #[cfg(test)]
 mod tests;
